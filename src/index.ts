c30321fb8cf3c61d8cc6c2a4aa401d9ad7ab1c6e
Kim T <kimturley@gmail.com>
2021-10-03 11:43:50 -0700

    Port over code from js version

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export * from './types';
 // Used for local development
 import { DateDecimal } from './classes';
 const today: DateDecimal = new DateDecimal();
-console.log(today.date.toString());
+console.log(today);
